ff57768821b73d59600c6dbf3cee8fd2a6e5fd8a
M. Kashwah <mohammed.kashwah@gmail.com>
2021-10-25 19:45:04 -0400

    axios get query..retrun html data of coinmarketcap

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const express = require ('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { response } = require('express')
 
 //instants
 dotenv.config()
@@ -11,14 +12,22 @@ const port = process.env.port
 
 
 
-//starting express
+//express routing
 app.get('/', (req, res) => {
-
     res.send('server is running_with nodemon')
 }
 )
-
 app.listen(port, () => {
-    console.log('listening to port ${port}')
+    console.log('listening to port %s', port)
 })
+
+
+//making axios get query
+app.get('/crypto', (req, res) =>{
+    axios.get('https://coinmarketcap.com/').then((response) =>{
+        const html_page = response.data
+        console.log(html_page)
+    })
+})
+
 
